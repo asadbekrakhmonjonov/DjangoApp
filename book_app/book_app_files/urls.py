6c52e17c08bf5b9131a9142bcd6348d9331a39dc
asadbekrakhmonjonov <143370851+asadbekrakhmonjonov@users.noreply.github.com>
2024-04-22 11:56:24 +0900

    Delete book_app directory

--- book_app/book_app_files/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .import views
-urlpatterns = [
-    path('', views.MainPage.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('books/', views.BookList.as_view()),
-
-]
